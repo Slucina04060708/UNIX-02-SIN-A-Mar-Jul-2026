0937ab4dd3708615756410155dbabad06a4cc28e
Slucina04060708 <salucinaar@uide.edu.ec>
2026-03-20 18:05:37 +0000

    Export public key in ASCII armor format

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -2,3 +2,4 @@ uname -a #daba información del ordenador
 which gpg #ubicación del binario de gpg
 gpg --version #me da la versión e infroamción adicional
 gpg --full-key-generate #genero llave pública y privada
+gpg --armor --export #exportar llave pública
