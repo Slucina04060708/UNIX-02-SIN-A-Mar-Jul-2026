9aba4cc6ab9e319babc4ebe597618f1a79df8f31
Slucina04060708 <salucinaar@uide.edu.ec>
2026-03-20 18:02:17 +0000

    Generate GnuPG public and private key pair

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,3 +1,4 @@
 uname -a #daba información del ordenador
 which gpg #ubicación del binario de gpg
 gpg --version #me da la versión e infroamción adicional
+gpg --full-key-generate #genero llave pública y privada
