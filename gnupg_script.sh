0ca0ecad6287a926fd8b4ad5066d6e0f8a500272
Slucina04060708 <salucinaar@uide.edu.ec>
2026-03-26 13:15:50 +0000

    Create doc_no_cifrado.txt for encryption test

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -11,3 +11,5 @@ push #envío los cambios
 gpg --armor --export-secret-keys XXXXXXXX #especificar el hash de llaves correspondientes
 gpg --armor --export-secret-keys #exportar privada
 gpg --armor --export slucina0406@gmail.com >mi_llave_pública.asc #exporto mi llave publica y la guardo en un archivo
+ gpg --import PAULA tu_llave_pública.asc #Importo la llave publica de mi compañera y creo un nombre para esa llave
+ echo "esta" >doc_no_cifrado.txt #echo "esta": Imprime la palabra "esta" en la terminal. >: Es un redireccionador. En lugar de mostrar el texto en pantalla, lo envía a un archivo. doc_no_cifrado.txt: Es el nombre del archivo que se creará (o se sobrescribirá si ya existe). 
