e8fdfd5c63d27c01d70801685011e02ff646aba1
Slucina04060708 <salucinaar@uide.edu.ec>
2026-04-09 12:24:53 +0000

    Create new directory structure for the project modules

--- script_boot.sh
+++ script_boot.sh
@@ -1,4 +1,4 @@
 git checkout <branch_name> #Switch to an existing branch to continue working on a specific feature or fix.
-Absolute route #
-Relative route #
-mkdir #
+Absolute route #Navigate using the full path from the root directory to ensure accuracy regardless of current location.
+Relative route #Reference a file or directory relative to the current working location using "./" or "../".
+mkdir <directory_name> #Create a new directory to organize project assets or source code.
