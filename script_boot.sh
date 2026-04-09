96f66310e16e1e732195107656013ff39200b336
Slucina04060708 <salucinaar@uide.edu.ec>
2026-04-09 12:40:36 +0000

    Standardize home directory navigation and environment pathing

--- script_boot.sh
+++ script_boot.sh
@@ -2,3 +2,7 @@ git checkout <branch_name> #Switch to an existing branch to continue working on 
 Absolute route #Navigate using the full path from the root directory to ensure accuracy regardless of current location.
 Relative route #Reference a file or directory relative to the current working location using "./" or "../".
 mkdir <directory_name> #Create a new directory to organize project assets or source code.
+cd #It takes you to the user's home directory or personal directory.
+cd ~ #Quickly return to the user's home directory using the tilde shortcut.
+cd /home/codespace #Move to the root directory of the codespace environment.
+echo $HOME #Print the value of the HOME environment variable to verify the user path.
