21fd14f8922006b6645e6f96582c0a31fc367ca3
Slucina04060708 <salucinaar@uide.edu.ec>
2026-04-27 03:04:27 +0000

    Document navigation commands and the difference between absolute and relative paths

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -1,3 +1,4 @@
+#BLOCK 1
 # General Syntax: command [options] [arguments]
 # command: The action (ls = list)
 # [options]: Flags that modify the output (usually start with -)
@@ -20,6 +21,7 @@ ls -rl
 ls -lr
 # All three commands do exactly the same: a detailed list in reverse order.
 
+#BLOCK 2
 # In order to execute the 'aptitude' commands from the course, 
 # the package must be installed first.
 
@@ -34,3 +36,34 @@ sudo apt install aptitude -y
 # aptitude -v moo     -> '-v' (verbose) Increases the detail of the response.
 # aptitude -vv moo    -> The program asks you to stop.
 # aptitude -vvv moo   -> The program gets annoyed.
+
+#BLOCK 3
+# pwd (Print Working Directory) = Shows your current location.
+pwd
+
+# cd (Change Directory) = Moves you to a different folder.
+cd [options] [path]
+
+# --- PATH TYPES ---
+
+# Absolute Path: Always starts from the root (/). 
+# It works no matter where you are currently located.
+cd /home/sysadmin
+
+# Relative Path: Starts from your current directory (does NOT start with /).
+# To go to 'Art' inside 'School' while being in 'Documents':
+cd School/Art
+
+# You can also do it step by step:
+cd School
+cd Art
+
+# --- SPECIAL CHARACTERS ---
+
+# .. (Double dot) = Represents the parent directory (one level up).
+cd ..
+
+# . (Single dot) = Represents the current directory.
+
+# ~ (Tilde) = Represents the current user's HOME directory.
+cd ~
