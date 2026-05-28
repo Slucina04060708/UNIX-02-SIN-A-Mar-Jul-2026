3103743a04583beecc10171a875f8b3aedaae660
Slucina04060708 <salucinaar@uide.edu.ec>
2026-05-28 12:56:50 +0000

    Establish primary group baseline using command substitution and create pre-newgrp control file

--- script.zsh
+++ script.zsh
@@ -1,7 +1,15 @@
-# See the actual principal group
+# Watch the actual principal group
 id
 id -gn # Only the nome of the principal group
 # Create a file and see which group is inhereted 
 touch ~/test_grupo_heredado.txt
 ls -la ~/test_grupo_heredado.txt
 # The group is the principal group of the user
+
+# Watch the actual group
+id -gn
+echo "Grupo actual: $(id -gn)"
+
+#Create a file before newgrp
+touch ~/antes_de_newgrp.txt
+ls -la ~/antes_de_newgrp.txt
