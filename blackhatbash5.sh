44ba6045b05d2d4a9e30286d5376133a9fb55ba1
Slucina04060708 <salucinaar@uide.edu.ec>
2026-06-05 17:17:34 +0000

    Implement I/O redirection script validating logical chaining, stream truncation, and stdout/stderr stream separation

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
-top #Spawns an interactive process layer that hijacks stdin/stdout. The shell suspends subsequent command parsing until the process exits.
-top& #Instructs the kernel to spawn the process asynchronously. The shell returns an immediate job ID and PID, leaving the terminal prompt available for continuous command ingestion.
+#top #Spawns an interactive process layer that hijacks stdin/stdout. The shell suspends subsequent command parsing until the process exits.
+#top& #Instructs the kernel to spawn the process asynchronously. The shell returns an immediate job ID and PID, leaving the terminal prompt available for continuous command ingestion.
+
+touch test && touch test123
+(ls; ps)
+ls; ps; whoami
+lzl || echo "el comando lzl falló"
+
+echo "Hola mundo!" > output.txt
+cat output.txt
+echo "Chao mundo!" > output.txt
+cat output.txt
+echo "Chao Universo" >> output.txt
+cat output.txt
+
+ls -l / &> stdout_and_stderr.txt
+ls -l / &>> stdout_and_stderr.txt
+
+ls -l / 1> stdout.txt 2> stderr.txt
+lzl 2> error.txt
+cat error.txt
