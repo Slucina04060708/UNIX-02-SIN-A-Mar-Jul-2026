ecb1d9a45e55840647cde339ebdc6f598df712af
Slucina04060708 <salucinaar@uide.edu.ec>
2026-06-09 16:52:32 +0000

    Implement iterative parameter loop using for-in structure

--- ping_with_arguments.sh
+++ ping_with_arguments.sh
@@ -7,3 +7,8 @@ echo "Pinging the target: ${TARGET}..."
 ping -c 5 "${TARGET}"
 echo "The arguments are: $@" #Stores all the arguments passed to the script as a single string.
 echo "The total number of arguments is: $#" #Counts the number of arguments passed to the script and returns that count as an integer.
+
+for args in "$@"; do
+echo "${args}"
+done
+# The above loop iterates through each argument passed to the script and prints it on a new line. The "$@" variable is used to access all the arguments as an array, allowing us to process each one individually.
