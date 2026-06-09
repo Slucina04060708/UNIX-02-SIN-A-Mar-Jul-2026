c08c329968cec7c04b31f96178a710215ce91646
Slucina04060708 <salucinaar@uide.edu.ec>
2026-06-09 16:46:02 +0000

    Using special variables \$@ for expansion and \$# for dynamic parameter counting

--- ping_with_arguments.sh
+++ ping_with_arguments.sh
@@ -5,3 +5,5 @@ TARGET="${1}"
 echo "Running the script ${SCRIPT_NAME}..."
 echo "Pinging the target: ${TARGET}..."
 ping -c 5 "${TARGET}"
+echo "The arguments are: $@" #Stores all the arguments passed to the script as a single string.
+echo "The total number of arguments is: $#" #Counts the number of arguments passed to the script and returns that count as an integer.
