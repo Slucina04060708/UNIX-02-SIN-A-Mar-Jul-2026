1f02c9c8e46b00a229b031580ac135e891871ea7
Slucina04060708 <salucinaar@uide.edu.ec>
2026-06-18 13:05:37 +0000

    Dynamic CSV translation script utilizing AWK 'OFS' variable overrides and standard output stream redirection

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -4,3 +4,18 @@ grep "35.237.4.214\|13.66.139.0" log.txt
 grep -e "35.237.4.214" -e "13.66.139.0" log.txt
 ps | grep TTY # Show processes associated with the current terminal
 ps | grep -i tty # Show processes associated with the current terminal, case insensitive
+grep -v "35.237.4.214" log.txt # Show lines that do not contain the IP address
+grep -o "35.237.4.214" log.txt # Show only the IP address matches
+awk '{print $1}' log.txt # Show only the first column
+awk '{print $1,$2,$3}' log.txt # Show the first three columns
+awk '{print $2}' log.txt # Show only the second column
+awk '{print $3}' log.txt # Show only the third column
+awk '{print $1,$NF}' log.txt
+
+cat << 'EOF' > registro.csv
+Santiago,Lucina,Quito
+Marcela,Garcia,Guayaquil
+William,Smith,New York
+EOF
+
+awk -F',' '{print $1}' registro.csv # Show only the first column from a CSV file
