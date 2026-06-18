525b18c75551a6c1db70b4fe213b04415ea4797b
Slucina04060708 <salucinaar@uide.edu.ec>
2026-06-18 12:42:55 +0000

    Auditing script utilizing pipeline streaming (|) and case-insensitive grep validation (-i)

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -2,3 +2,5 @@
 grep "35.237.4.214" log.txt
 grep "35.237.4.214\|13.66.139.0" log.txt
 grep -e "35.237.4.214" -e "13.66.139.0" log.txt
+ps | grep TTY # Show processes associated with the current terminal
+ps | grep -i tty # Show processes associated with the current terminal, case insensitive
